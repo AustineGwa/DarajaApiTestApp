f261e465271a8c25e28d00162bcae2a4a85ba3eb
samuelowino <samuelowino43@yahoo.com>
2019-06-30 16:06:36 +0300

    dialog glitch fixes

--- app/src/main/java/com/example/darajaapitestapp/MainActivity.java
+++ app/src/main/java/com/example/darajaapitestapp/MainActivity.java
@@ -24,7 +24,7 @@ import com.androidstudy.daraja.util.TransactionType;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText amount,account;
+    private EditText amount, account;
     private Button stkpushBtn;
     private Daraja daraja;
     private String mPesaResult;
@@ -54,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Error " + error, Toast.LENGTH_SHORT).show();
             }
         });
+
+        genericLoader = new GenericLoader();
     }
 
     @Override
@@ -67,10 +69,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
 
-                if (TextUtils.isEmpty(amount.getText().toString())  || TextUtils.isEmpty(account.getText().toString())) {
+                if (TextUtils.isEmpty(amount.getText().toString()) || TextUtils.isEmpty(account.getText().toString())) {
                     Toast.makeText(getApplicationContext(), "All  Fields are required ", Toast.LENGTH_SHORT).show();
-                } else {
-                    //dialog.show();
+                }
 
 
                 new AsyncTask<Void, Void, String>() {
@@ -79,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
                     protected void onPreExecute() {
                         super.onPreExecute();
 
-                        new GenericLoader().show(getSupportFragmentManager(),"please wait");
-                       // Toast.makeText(getApplicationContext(),"started call to mpesa ...",Toast.LENGTH_SHORT).show();
+                        genericLoader.show(getSupportFragmentManager(), "tag");
+                        // Toast.makeText(getApplicationContext(),"started call to mpesa ...",Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
@@ -89,64 +90,64 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     @Override
-                    protected void onPostExecute(String  aMessage) {
+                    protected void onPostExecute(String aMessage) {
                         super.onPostExecute(aMessage);
-                       // new GenericLoader().dismiss();
-                        Toast.makeText(getApplicationContext(),aMessage,Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), aMessage, Toast.LENGTH_SHORT).show();
                     }
                 }.execute();
             }
 
-            }
-        });
 
+        });
 
 
     }
 
-    private String  initiateStkPush() {
+    private String initiateStkPush() {
 
-            //Get Phone Number from User Input
+        //Get Phone Number from User Input
 
-            String amountSent = amount.getText().toString().trim();
-            String user_account_name = account.getText().toString();
-
-
-            //TODO :: REPLACE WITH YOUR OWN CREDENTIALS  :: THIS IS SANDBOX DEMO
-            LNMExpress lnmExpress = new LNMExpress(
-                    "174379",
-                    "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",  //https://developer.safaricom.co.ke/test_credentials
-                    TransactionType.CustomerPayBillOnline,
-                    amountSent,
-                    "254718728894",
-                    "174379",
-                    "0718728894",
-                    "http://mycallbackurl.com/checkout.php",
-                    "user_account_name",
-                    "API TESTING"
-            );
-
-            daraja.requestMPESAExpress(lnmExpress,
-                    new DarajaListener<LNMResult>() {
-                        @Override
-                        public void onResult(@NonNull LNMResult lnmResult) {
-                            Log.i(MainActivity.this.getClass().getSimpleName(), lnmResult.ResponseDescription);
-                            mPesaResult = "Success "+ lnmResult.ResponseDescription;
-
-                        }
-
-                        @Override
-                        public void onError(String error) {
-                            Log.i(MainActivity.this.getClass().getSimpleName(), error);
-                            mPesaResult = "Error : " +error;
+        String amountSent = amount.getText().toString().trim();
+        String user_account_name = account.getText().toString();
 
 
-                        }
+        //TODO :: REPLACE WITH YOUR OWN CREDENTIALS  :: THIS IS SANDBOX DEMO
+        LNMExpress lnmExpress = new LNMExpress(
+                "174379",
+                "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",  //https://developer.safaricom.co.ke/test_credentials
+                TransactionType.CustomerPayBillOnline,
+                amountSent,
+                "phone-number",
+                "174379",
+                "phone-number",
+                "http://mycallbackurl.com/checkout.php",
+                "user_account_name",
+                "API TESTING"
+        );
+
+        daraja.requestMPESAExpress(lnmExpress,
+                new DarajaListener<LNMResult>() {
+                    @Override
+                    public void onResult(@NonNull LNMResult lnmResult) {
+                        Log.i(MainActivity.this.getClass().getSimpleName(), lnmResult.ResponseDescription);
+                        mPesaResult = "Success " + lnmResult.ResponseDescription;
+                        genericLoader.dismiss();
+
                     }
-            );
+
+                    @Override
+                    public void onError(String error) {
+                        Log.i(MainActivity.this.getClass().getSimpleName(), error);
+                        mPesaResult = "Error : " + error;
+                        genericLoader.dismiss();
 
 
-            return mPesaResult;
+                    }
+                }
+        );
+
+
+        return mPesaResult;
 
     }
 }
