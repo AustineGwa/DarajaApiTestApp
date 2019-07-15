a83136495a6da42f19d97fb78a65ee50f4efd850
AustineGwa <austinegwa64@gmail.com>
2019-07-15 06:26:20 +0300

    modified callbackurl file to decode response into a php associative array that can be used programmatically

--- mpesa-callback-url.php
+++ mpesa-callback-url.php
@@ -3,23 +3,22 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header('Access-Control-Allow-Headers: Origin, X-Requested-With,X-Auth-Token, Content-Type, Accept');
 
-
+/*
+** Read Response  from mpesa 
+*/
   $stkCallbackResponse = file_get_contents('php://input');
-  $logFile = "stkPushCallbackResponse.json";
-  $log = fopen($logFile, "a");
-  fwrite($log, $stkCallbackResponse);
-  fclose($log);
+
+  //Log  the mpesa response message to a jsonfile
+  // $logFile = "stkPushCallbackResponse.json";
+  // $log = fopen($logFile, "a");
+  // fwrite($log, $stkCallbackResponse);
+  // fclose($log);
 
 
-// $callbackData = $mpesa->getDataFromCallback();
-// $my_file = 'response.txt';
-// $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-// fwrite($handle, $callbackData);
-// If validation fails, pass false to finishTransaction()
-// $mpesa= new \Safaricom\Mpesa\Mpesa();
-// $callbackData=$mpesa->finishTransaction(false
-// $mpesa= new \Safaricom\Mpesa\Mpesa();
-// If validation fails, pass false to finishTransaction()
-// $mpesa= new \Safaricom\Mpesa\Mpesa();
-// $callbackData=$mpesa->finishTransaction(false)
+  //decode the json response into php associative array.
+  // Convert to array 
+$array = json_decode($stkCallbackResponse, true);
+
+
+
 ?>
