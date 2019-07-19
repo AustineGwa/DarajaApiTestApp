52e9ca536f19ec3e0945b8aaf8281f393d51a9ac
AustineGwa <austinegwa64@gmail.com>
2019-07-19 03:53:59 +0300

    persisting  callback url data to database

--- mpesa-callback-url.php
+++ mpesa-callback-url.php
@@ -1,24 +1,58 @@
-<?php 
+<?php
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header('Access-Control-Allow-Headers: Origin, X-Requested-With,X-Auth-Token, Content-Type, Accept');
 
+
+$stkCallbackResponse = file_get_contents('php://input');
+
 /*
-** Read Response  from mpesa 
+$logFile = "logs/IPN-".time().".json";
+$log = fopen($logFile, "a");
+fwrite($log, $stkCallbackResponse);
+fclose($log);
 */
-  $stkCallbackResponse = file_get_contents('php://input');
 
-  //Log  the mpesa response message to a jsonfile
-  // $logFile = "stkPushCallbackResponse.json";
-  // $log = fopen($logFile, "a");
-  // fwrite($log, $stkCallbackResponse);
-  // fclose($log);
+$ipn_data = json_decode($stkCallbackResponse,true);
+
+$new_status = 'COMPLETED';
+
+$checkout_request_id = $ipn_data['Body']['stkCallback']['CheckoutRequestID'];
+
+$result_code = $ipn_data['Body']['stkCallback']['ResultCode'];
+
+$mpesa_confirmation_code = $ipn_data['Body']['stkCallback']['CallbackMetadata']['Item']['1']['Value'];
+
+if($result_code != 0){
+
+	$new_status = 'CANCELLED';
+}
 
 
-  //decode the json response into php associative array.
-  // Convert to array 
-$array = json_decode($stkCallbackResponse, true);
+//Process Database Saving of data
+include 'lib/class.connect.php';
 
 
+$dbConnect = new Connect();
 
+$dbConnect = $dbConnect->db();
+
+
+//Check If signature matches
+
+
+		  			
+$save = mysqli_query($dbConnect,
+"UPDATE
+  mpesa_transactions
+SET
+  txn_status = '$new_status',
+  mpesa_confirmation_id = '$mpesa_confirmation_code'
+WHERE
+  checkout_request_id = '$checkout_request_id'") or mysqli_error($dbConnect);
+
+
+$dbConnect->close();
+exit();
 ?>
